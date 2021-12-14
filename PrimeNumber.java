6c7d5c065b43698cf561cb4f7a0f0d081c797984
Thejeswararao <thejeswararaom@gmail.com>
2021-12-14 20:42:42 +0530

    Added:PrimeNumber

---
+++ PrimeNumber.java
@@ -0,0 +1,26 @@
+package bridgelabz.com;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		System.out.println("Enter a Number .");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		scanner.close();
+		int counter = 0;
+		for (int i = 2; i <= number / 2; i++) {
+
+			if (number % i == 0) {
+				counter = 1;
+				;
+				break;
+			}
+		}
+
+		if (counter == 0)
+			System.out.println(number + " is a prime number.");
+		else
+			System.out.println(number + " is not a prime number.");
+	}
+}
